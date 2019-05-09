030ebb3ad98876a41ad278e360b555d19ec4476d
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-05-09 11:38:18 +0200

    Fixed gravity

--- Assets/Prototype1/Scripts/PlayerController.cs
+++ Assets/Prototype1/Scripts/PlayerController.cs
@@ -68,10 +68,11 @@ public class PlayerController : MonoBehaviour
         {
             //Move
             Vector3 moveDirection = new Vector3(Input.GetAxisRaw(input.Horizontal), 0.0f, -Input.GetAxisRaw(input.Vertical));
-            rigidbody.velocity = moveDirection * 100 * speed * Time.deltaTime;
+            Vector3 velocity = (moveDirection * 100 * speed * Time.deltaTime) + (Vector3.up * rigidbody.velocity.y);
+            rigidbody.velocity = velocity;
 
             //Turn character
-            if(moveDirection.sqrMagnitude > 0.0f)
+            if (moveDirection.sqrMagnitude > 0.0f)
             {
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection, Vector3.up), rotationSpeed);
             }
