924ed0be1841da64e6190f7cc695a73653748127
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-05-10 15:27:13 +0200

    Added camera zoomback and a second level to test it

---
+++ Assets/Prototype1/Scripts/CameraZoomAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomAnim : MonoBehaviour
+{
+    Animator anim;
+    /*[HideInInspector] */
+    public bool isZoomed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("playerInside", isZoomed);
+    }
+}
---
+++ Assets/Prototype1/Scripts/CameraZoomBack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomBack : MonoBehaviour
+{
+
+    public CameraZoomAnim mainCamera;
+
+    public bool playerInside = false;
+    void Start()
+    {
+        mainCamera = Camera.main.GetComponent<CameraZoomAnim>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerInside = true;
+            mainCamera.isZoomed = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInside = true;
+            mainCamera.isZoomed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInside = false;
+            mainCamera.isZoomed = false;
+        }
+    }
+
+}
